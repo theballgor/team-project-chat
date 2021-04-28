64d896bed5f432bf40dca2aaceeed31970bb005c
roman1273232 <74620900+roman1273232@users.noreply.github.com>
2021-04-28 17:44:37 +0300

    Added basic database tables. Installed Entity Framework and Material Design

---
+++ ClientServerLibrary/DbClasses/Conversation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientServerLibrary.DbClasses
+{
+    [Serializable]
+    public enum ConversationStatus
+    {
+        pending,
+        confirmed,
+        blocked
+    }
+    [Table("Conversations")]
+    [Serializable]
+    class Conversation
+    {
+        [Key]
+        public int Id;
+        public string Theme;
+        public string Avatart;
+        public ConversationStatus ConversationStatus;
+        public string StreamingPort;
+        // user_id in tz
+        public string host_id;
+    }
+}
