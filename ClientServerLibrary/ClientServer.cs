9d2af4e19ff9477e4a700e4a0c74bbddab9ebe24
Luntik7 <svv3888@gmail.com>
2021-05-05 17:03:18 +0300

    Створив класс з базовою логікою ClientModel, добавив загальні класи для взаємодії клієнта та сервера

---
+++ ClientServerLibrary/ClientServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer
+{
+    [Serializable]
+    public enum MessageType
+    {
+        Text,
+        Audio,
+        File
+    }
+
+    [Serializable]
+    public struct ClientServerMessage
+    {
+        public ClientServerMessage(
+            object content,
+            object additionalContent,
+            MessageType messageType,
+            DateTime date)
+        {
+            Content = content;
+            AdditionalContent = additionalContent;
+            MessageType = messageType;
+            Date = date;
+        }
+
+        public object Content { get; set; }
+        public object AdditionalContent { get; set; }
+        public MessageType MessageType { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public static class GlobalVariables
+    {
+        public static readonly int ServerPort = 40000;
+        public static readonly IPAddress LocalIP = IPAddress.Parse("127.0.0.1");
+    }
+}
