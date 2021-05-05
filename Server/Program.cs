161e645ddd0a00726bdcacc7ccc53b5c5da1f116
Igor Baluk <theballgor@gmail.com>
2021-05-05 17:03:33 +0300

    Створив клас-сервер ServerClass з базовою логікою

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            ServerClass server = new ServerClass(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000));
+            server.Connect();
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+                Console.ReadLine();
         }
     }
 }
