2e860b51ecaebe037cf5b6a074249195eb42b068
roman1273232 <74620900+roman1273232@users.noreply.github.com>
2021-05-11 11:53:42 +0300

    Mofify db

--- Server/Database/ChatDBContext.cs
+++ Server/Database/ChatDBContext.cs
@@ -4,8 +4,9 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClientServerLibrary.DbClasses;
 
-namespace ClientServerLibrary.DbClasses
+namespace Server.Database
 {
     public class ChatDBContext : DbContext
     {
