93b3f33f7350f00e9b397e106475dae0c668390a
Igor Baluk <theballgor@gmail.com>
2021-05-17 21:43:57 +0300

    Редагував логін модель та попраював на DataWorker

--- Client/Model/DataWorker.cs
+++ Client/Model/DataWorker.cs
@@ -5,26 +5,63 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ClientServerLibrary;
+using ClientServerLibrary.DbClasses;
 
 namespace Client.Model
 {
-    static class DataWorker
+    public static class DataWorker
     {
-        private static ClientServerMessage message;
-
         static DataWorker()
         {
             ClientModel.CreateClientEndpoint(GlobalVariables.LocalIP, ClientModel.GetFreeTcpPort());
             ClientModel.Connect(GlobalVariables.LocalIP, GlobalVariables.ServerPort);
-            Task.Run(() => {
-                ClientModel.StartListening(ref message);
-            });
+            while (true)
+                Handler();
         }
 
 
-        public static ClientServerMessage getMessage()
+
+
+        public static void Test()
         {
-            return message;
+
         }
+
+
+        private static  void Handler()
+        {
+            foreach (ClientServerMessage item in ClientModel.Messages)
+            {
+                Task.Run(() => 
+                {
+                    switch (item.ActionType)
+                    {
+                        case ActionType.SendText:
+                            break;
+                        case ActionType.SendAudio:
+                            break;
+                        case ActionType.SendFile:
+                            break;
+                        case ActionType.RegisterUser:
+                            break;
+                        case ActionType.LogInUser:
+                            LoginModel.LoginUser((User)item.Content);
+                            ClientModel.Messages.Remove(item);
+                            break;
+                        case ActionType.CreateConversation:
+                            break;
+                        case ActionType.GetConversationMessages:
+                            break;
+                        case ActionType.Error:
+                            break;
+                        case ActionType.Success:
+                            break;
+                        default:
+                            break;
+                    }
+                });
+            }
+        }
+
     }
 }
--- Client/Model/LoginModel.cs
+++ Client/Model/LoginModel.cs
@@ -3,14 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClientServerLibrary;
+using ClientServerLibrary.DbClasses;
+using ClientLibrary;
 
 namespace Client.Model
 {
-    class LoginModel
+     class LoginModel
     {
-        private ClientModel client;
+        private string email;
+        private string password;
 
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
 
+        public void Send()
+        {
+            DataWorker.Test();
 
+            User user = new User {
+                Email = email,
+                Password = Cryptography.Encrypt(password),
+                Avatar = null,
+                ConversationConnections = null,
+                Description = null,
+                Id = -1,
+                PhoneNumber = null,
+                Status = UserStatus.Online,
+                Username = null
+            };
+
+            ClientServerMessage message = new ClientServerMessage { Content = user };
+            message.ActionType = ActionType.LogInUser;
+        }
+
+        public void LoginUser(User user)
+        {
+            Console.WriteLine(user.Email);
+        }
+
+        public void Validate()
+        {
+            ValidateString(password, "Invalid data", 8, 16);
+            ValidateEmail("Invalid data");
+        }
+
+        private void ValidateString(string str, string exceptionMessage, int from, int to)
+        {
+            if (string.IsNullOrEmpty(str) || (str.Length < from || str.Length > to))
+                throw new ArgumentException(exceptionMessage);
+        }
+
+        private void ValidateEmail(string exceptionMessage)
+        {
+            try
+            {
+                System.Net.Mail.MailAddress m = new System.Net.Mail.MailAddress(email);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
     }
 }
